ff3726cb2316ff5e63d13906df88defac29dbc1e
Dmitriy Zolotykh <dm.zolotykh@gmail.com>
2024-04-30 00:19:48 +0300

    add amogus

--- include/server/handlers/AmogusHandler.h
+++ include/server/handlers/AmogusHandler.h
@@ -3,15 +3,20 @@
 
 #include <server/handlers/IHandler.h>
 #include <socket/ClientSocket.h>
+#include <thread>
+#include <fstream>
+#include <string>
+#include <vector>
 
 namespace Server::Handlers::Sus {
     class AmogusHandler final : public IHandler {
     public:
-        AmogusHandler(const ClientSocket& client) {
+        AmogusHandler(const ClientSocket& client): client(client) {
             client.send_bytes("AMOGUS\n");
         };
         Response handle() override;
     private:
+        const ClientSocket& client;
         const std::string SUSSY_BAKA =
                                  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀ \n"
                                  "⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀ \n"
--- src/server/handlers/AmogusHandler.cpp
+++ src/server/handlers/AmogusHandler.cpp
@@ -1,5 +1,29 @@
 #include <server/handlers/AmogusHandler.h>
 
 Server::Response Server::Handlers::Sus::AmogusHandler::handle() {
-    return Server::create_response(SUSSY_BAKA);
+    client.send_bytes(SUSSY_BAKA);
+
+    std::ifstream file("amogus.txt");
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::vector<std::string> frames;
+    std::string buff;
+    for (std::string line; std::getline(file, line);) {
+        frames.push_back(line);
+    }
+
+    while (true) {
+        int cnt = 0;
+        for (const auto& frame : frames) {
+            client.send_bytes(frame);
+            client.send_bytes("\n");
+            cnt++;
+            if (cnt == 26) {
+                cnt = 0;
+                for (int i = 0; i < 26; i++) {
+                    client.send_bytes("\033[A");
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            }
+        }
+    }
 }
